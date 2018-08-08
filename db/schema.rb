5610851bd5f806f4162ff3d4888de78eac531c25
Aulia Rifkyano <rifkyanoa@gmail.com>
2018-08-08 08:21:45 +0000

    create new template, action and route

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20180808043744) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "create_at"
+    t.datetime "created_at"
     t.datetime "updated_at"
   end
 
